882f4333bb2dc9d928a04a809ffdfe9ea32a566f
AhmadJamshaid <ahmadjamshaid2003@gmail.com>
2026-04-17 14:16:49 +0500

    Add files via upload

---
+++ Feature_Extraction_Training_Testing (1).py
@@ -0,0 +1,179 @@
+[Event "106th ch-Marshall CC 2022"]
+[Site "New York USA"]
+[Date "2022.12.10"]
+[Round "3.2"]
+[White "Tanenbaum,Zachary"]
+[Black "unknown"]
+[Result "1/2-1/2"]
+[WhiteElo "2188"]
+[BlackElo "2403"]
+[ECO "B35"]
+
+1.e4 c5 2.Nf3 g6 3.d4 cxd4 4.Nxd4 Nc6 5.Nc3 Bg7 6.Be3 Nf6 7.Bc4 O-O 8.Bb3 d6
+9.h3 Na5 10.O-O b6 11.Qd2 Bb7 12.f3 Rc8 13.Qd3 a6 14.a4 Nd7 15.Rad1 Ne5 16.Qe2 Nec4
+17.Bxc4 Nxc4 18.Bc1 Qc7 19.Qf2 e6 20.b3 Na5 21.Bb2 Rfd8 22.Rd3 d5 23.exd5 Bxd5
+24.Nxd5 Rxd5 25.c4 Rd7 26.Rfd1 Rcd8 27.Kh1 h5 28.Qe2 Kh7 29.Qc2 Rd6 30.Bc3 Nc6
+31.Nxc6 Qxc6 32.Bxg7 Kxg7 33.Qc3+ Kg8 34.Rxd6 Rxd6 35.Rxd6 Qxd6 36.a5 Qd1+
+37.Kh2 Qd6+ 38.Kh1 Qd1+ 39.Kh2 Qd6+ 40.Kh1  1/2-1/2
+
+[Event "106th ch-Marshall CC 2022"]
+[Site "New York USA"]
+[Date "2022.12.10"]
+[Round "2.2"]
+[White "unknown"]
+[Black "Grasso,D"]
+[Result "1/2-1/2"]
+[WhiteElo "2403"]
+[BlackElo "2158"]
+[ECO "C24"]
+
+1.e4 e5 2.Bc4 Nf6 3.d3 c6 4.Nf3 d5 5.Bb3 a5 6.a4 Bd6 7.exd5 Nxd5 8.O-O O-O
+9.Re1 Bg4 10.h3 Bh5 11.Nbd2 Nd7 12.Ne4 Bb4 13.c3 Be7 14.Ng3 Bg6 15.Nxe5 Nxe5
+16.Rxe5 Bd6 17.Re1 Bxg3 18.fxg3 Bxd3 19.Kh2 Bg6 20.Qd4 Nf6 21.Be3 Re8 22.Rad1 Qxd4
+23.Bxd4 Nd5 24.Kg1 h5 25.Kf2 Bf5 26.Kf3 Be6 27.Re5 Nxc3 28.Bxc3 Bxb3 29.Rxe8+ Rxe8
+30.Rd7  1/2-1/2
+
+
+[Event "106th ch-Marshall CC 2022"]
+[Site "New York USA"]
+[Date "2022.12.09"]
+[Round "1.2"]
+[White "Meduri,Aakaash"]
+[Black "unknown"]
+[Result "0-1"]
+[WhiteElo "2020"]
+[BlackElo "2403"]
+[ECO "A45"]
+
+1.d4 Nf6 2.Nc3 d5 3.Bf4 g6 4.e3 Bg7 5.Be2 c5 6.Nb5 Na6 7.c3 O-O 8.h4 h6 9.Nf3 Bg4
+10.Be5 Ne4 11.Bxg7 Kxg7 12.dxc5 Naxc5 13.Qd4+ f6 14.Rd1 e5 15.Qxd5 Qe7 16.Qc4 Rfc8
+17.Nd6 Bxf3 18.Nxc8 Bxe2 19.Qxe2 Rxc8 20.h5 g5 21.Qc2 f5 22.f3 Nd6 23.b4 Ne6
+24.Qd3 Rd8 25.O-O Ne4 26.Qc2 Ng3 27.Rxd8  0-1
+
+[Event "104th ch-Marshall CC 2020"]
+[Site "chess.com INT"]
+[Date "2020.12.22"]
+[Round "8"]
+[White "Colas,Joshua"]
+[Black "unknown"]
+[Result "0-1"]
+[WhiteElo "2288"]
+[BlackElo "2420"]
+[ECO "A04"]
+
+1.Nf3 c5 2.c4 Nc6 3.b3 e5 4.Bb2 d6 5.e3 g6 6.d4 exd4 7.exd4 Bg7 8.Bc3 d5
+9.cxd5 Qxd5 10.dxc5 Qxd1+ 11.Kxd1 Nf6 12.Bd3 O-O 13.Kc2 Nd7 14.Bxg7 Kxg7
+15.Kb2 Nxc5 16.Bc2 Nb4 17.Ne1 Nxc2 18.Kxc2 Bf5+ 19.Kb2 Rfe8 20.Nc3 Rad8 21.b4 Ne6
+22.f3 Rd2+ 23.Kb3 Nd4+ 24.Ka4 Re3  0-1
+
+[Event "104th ch-Marshall CC 2020"]
+[Site "chess.com INT"]
+[Date "2020.12.21"]
+[Round "6"]
+[White "unknown"]
+[Black "Rosenberg,E"]
+[Result "1-0"]
+[WhiteElo "2420"]
+[BlackElo "2125"]
+[ECO "B11"]
+
+1.e4 c6 2.Nc3 d5 3.Nf3 Bg4 4.h3 Bh5 5.exd5 cxd5 6.Bb5+ Nc6 7.g4 Bg6 8.Ne5 Rc8
+9.d4 e6 10.Qe2 Bb4 11.h4 Ne7 12.h5 Be4 13.f3 O-O 14.Nxc6 Nxc6 15.Be3 Qf6
+16.fxe4 Nxd4 17.Bxd4 Qxd4 18.Rd1 Qf6 19.exd5 Rxc3 20.bxc3 Bxc3+ 21.Rd2 Bxd2+
+22.Kxd2 Qg5+ 23.Kc3 Qxd5 24.Rd1 Qxa2 25.Bc4 Qa3+ 26.Bb3 h6 27.Kd2 Qb4+ 28.Kc1 Rc8
+29.Rd7 a5 30.Qf3 Rf8 31.Rxb7 Qd4 32.c3 Qc5 33.Kb2 Qe5 34.Bc2 f5 35.Qd3 Rb8
+36.Rxb8+ Qxb8+ 37.Ka2 Qe5 38.gxf5 exf5 39.Qxf5 Qxc3 40.Bb3+ Kh8 41.Qf8+  1-0
+
+[Event "104th ch-Marshall CC 2020"]
+[Site "chess.com INT"]
+[Date "2020.12.19"]
+[Round "3"]
+[White "Azarov,S"]
+[Black "unknown"]
+[Result "1/2-1/2"]
+[WhiteElo "2604"]
+[BlackElo "2420"]
+[ECO "B36"]
+
+1.e4 c5 2.Nf3 g6 3.d4 cxd4 4.Nxd4 Nc6 5.c4 Nf6 6.Nc3 d6 7.Be2 Nxd4 8.Qxd4 Bg7
+9.Be3 O-O 10.Qd3 Bd7 11.O-O a5 12.b3 Bc6 13.Bd4 Nd7 14.Bxg7 Kxg7 15.Kh1 Qb6
+16.Rab1 Qc5 17.f4 Nf6 18.Nd5 Nxe4 19.Qxe4 e6 20.Bf3 exd5 21.cxd5 Bd7 22.Rbc1 Qb6
+23.Qe2 Rac8 24.Qb2+ f6 25.Rce1 Rce8 26.h3 h5 27.a3 Rxe1 28.Rxe1 Re8 29.Rxe8 Bxe8
+30.Qc3 Bf7  1/2-1/2
+
+
+[Event "104th ch-Marshall CC 2020"]
+[Site "chess.com INT"]
+[Date "2020.12.19"]
+[Round "1"]
+[White "Lins,Cheryl"]
+[Black "unknown"]
+[Result "0-1"]
+[WhiteElo ""]
+[BlackElo "2420"]
+[ECO "A34"]
+
+1.c4 c5 2.Nc3 g6 3.Nf3 Bg7 4.d4 cxd4 5.Nxd4 Nc6 6.Nxc6 bxc6 7.e4 d6 8.Be3 Nf6
+9.Be2 O-O 10.O-O Be6 11.Rc1 Qa5 12.Qa4 Qxa4 13.Nxa4 Nxe4 14.f3 Nf6 15.Rfd1 c5
+16.Nc3 Rab8 17.b3 Nd7 18.Nb5 Rb7 19.f4 Nb8 20.Bf3 Rd7 21.Nc3 Rc8 22.Nd5 Nc6
+23.Bf2 Nd4 24.Rd3 Bf5 25.Re3 e6 26.Nc3 Nc2 27.g4 Nxe3 28.Bxe3 Bd3 29.Kf2 f5
+30.g5 Bxc3 31.Rxc3 Be4 32.Be2 a5 33.a4 Kf7 34.Bd2 Ra8 35.Rh3 Kg8 36.Rg3 Rb7
+37.Bd1 d5 38.cxd5 Bxd5 39.Rc3 Rc8 40.Be3 c4 41.bxc4 Rxc4 42.Rd3 Rb2+ 43.Kg3 Rc7
+44.Bd4 Ra2 45.Bb6 Rb7 46.Bd4 Rb1 47.Kh3 Bc4  0-1
+
+
+[Event "World Senior Teams +50"]
+[Site "Dresden GER"]
+[Date "2018.07.15"]
+[Round "9.2"]
+[White "Barron,M"]
+[Black "unknown"]
+[Result "0-1"]
+[WhiteElo "2123"]
+[BlackElo "2468"]
+[ECO "B70"]
+
+1.e4 c5 2.Nf3 d6 3.d4 cxd4 4.Nxd4 Nf6 5.Nc3 g6 6.Be2 Bg7 7.O-O O-O 8.Kh1 Nc6
+9.Nb3 a6 10.f4 b5 11.Bf3 Bb7 12.Nd5 Nd7 13.c3 e6 14.Ne3 Qc7 15.Qe2 a5 16.Rd1 b4
+17.Nd4 Nxd4 18.cxd4 e5 19.dxe5 dxe5 20.Nd5 Bxd5 21.exd5 Qd6 22.g3 Rfe8 23.fxe5 Bxe5
+24.Qg2 Kg7 25.Rb1 Rac8 26.b3 h5 27.Qd2 Rh8 28.Bb2 h4 29.Kg2 Bc3 30.Rbc1 Qf6
+31.Bxc3 Rxc3 32.Rxc3 bxc3 33.Qf4 Ne5 34.Be4 h3+ 35.Kg1 Qb6+ 36.Qf2 Qxf2+
+37.Kxf2 f5 38.Bd3 Ng4+ 39.Kg1 Ne3 40.Rc1 Nxd5 41.Bc4 Rd8 42.Kf2 Nf6 43.Ke3 Ng4+
+44.Kf4 Rd4+ 45.Kf3 Ne5+ 46.Ke3 Re4+ 47.Kf2 Rxc4  0-1
+
+[Event "100th Edward Lasker Mem"]
+[Site "New York USA"]
+[Date "2016.12.11"]
+[Round "5"]
+[White "Allicock,R"]
+[Black "unknown"]
+[Result "1/2-1/2"]
+[WhiteElo "2151"]
+[BlackElo "2508"]
+[ECO "B27"]
+
+1.e4 c5 2.Nf3 g6 3.d4 cxd4 4.Qxd4 Nf6 5.e5 Nc6 6.Qa4 Nd5 7.Qe4 Ndb4 8.Bb5 a6
+9.Ba4 b5 10.a3 d5 11.Qf4 bxa4 12.axb4 Bf5 13.c3 Bg7 14.O-O O-O 15.Re1 Qb8
+16.Nbd2 Bd7 17.Nf1 a5 18.bxa5 Rxa5 19.Ng3 Qb3 20.Qh4 Qc4 21.Bf4 Rb5 22.Nf1 Rxb2
+23.Ne3 Qb5 24.Bh6 f6 25.Bxg7 Kxg7 26.exf6+ exf6 27.Qxa4 Qxa4 28.Rxa4  1/2-1/2
+
+[Event "100th Edward Lasker Mem"]
+[Site "New York USA"]
+[Date "2016.12.09"]
+[Round "1"]
+[White "Shtivelband,R"]
+[Black "unknown"]
+[Result "1/2-1/2"]
+[WhiteElo "2135"]
+[BlackElo "2508"]
+[ECO "B42"]
+
+1.e4 c5 2.Nf3 e6 3.d4 cxd4 4.Nxd4 a6 5.Bd3 Bc5 6.Nb3 Be7 7.c4 d6 8.Nc3 Nf6
+9.O-O O-O 10.Be3 b6 11.f4 Bb7 12.Qf3 Re8 13.Rad1 Qc7 14.Qh3 Nbd7 15.Kh1 Rad8
+16.Nd2 Bf8 17.Rc1 Qb8 18.Bb1 h6 19.Rce1 b5 20.cxb5 axb5 21.Nxb5 Ba6 22.a4 Bxb5
+23.axb5 Qxb5 24.Bd4 d5 25.e5 Nh7 26.Nf3 Bb4 27.Rd1 Nhf8 28.g4 Bc5 29.Bc3 Qb7
+30.Nd4 Rc8 31.Rg1 Be7 32.Rde1 Nc5 33.g5 hxg5 34.fxg5 Ne4 35.Rxe4 dxe4 36.Rg4 Rxc3
+37.Qxc3 Rc8 38.Bxe4 Rxc3 39.Bxb7 Rc1+ 40.Kg2 Rb1 41.b3 Rb2+ 42.Kh3 Bc5 43.Ba6 Ng6
+44.Bc4 Nxe5 45.Re4 Bxd4 46.Rxd4 Kh7 47.Kg3 Kg6 48.h4 Kf5 49.Rf4+ Kg6 50.Rd4  1/2-1/2
+
+
---
+++ testing_unseen (2).py
@@ -0,0 +1,208 @@
+[Event "New York"]
+[Site "New York"]
+[Date "1931.??.??"]
+[Round "6"]
+[White "Turover, Isador Samuel"]
+[Black "unknown"]
+[Result "1/2-1/2"]
+[WhiteElo ""]
+[BlackElo ""]
+[ECO "D50"]
+
+1.d4 d5 2.c4 e6 3.Nc3 Nf6 4.Bg5 Bb4 5.e3 c5 6.Bxf6 Qxf6 7.a3 cxd4 8.exd4 Bxc3+
+9.bxc3 O-O 10.Nf3 Nc6 11.cxd5 exd5 12.Be2 Be6 13.O-O Rac8 14.Qa4 a6 15.Rab1 b5
+16.Qc2 Nxd4 17.Nxd4 Qxd4 18.Qxh7+ Kxh7 19.cxd4 Rc3 20.a4 bxa4 21.Rb4 a3 22.Ra4 Rb8
+23.Ra1 Bd7 24.Rxa6 Rb4 25.h3 Ra4 26.Rxa4 Bxa4 27.Ba6 Bc2 28.Bb7 Rd3 29.Bxd5 Kg8
+30.Bf3 Rb3 31.Kh2 Bd3 32.Bd5 Rc3 33.f3 Bf5 34.Kg3 Rd3 35.Bc4 Rc3 36.Bd5 g5
+37.h4 gxh4+ 38.Kxh4 Rd3 39.Kg3 Kg7 40.Bc4 Rxd4 41.Bb3 Bg6 42.Bxf7 Bxf7 43.Rxa3 Rd2
+44.Ra4 Be6 45.Rf4 Kg6 46.Kh2 Kg5 47.Kg3 Rc2 48.Rd4 Bc4 49.Rg4+ Kf5 50.Kh2 Bf7
+51.Kg3 Bd5 52.Rh4 Ke5 53.Rg4 Bc4 54.Kh2 Bf1 55.Kg1 Be2 56.Kf2 Kf5 57.Kg3 Bf1
+58.Kh2 Ra2 59.Kg1 Be2 60.Kh1 Bd1 61.Kg1 Be2  1/2-1/2
+
+[Event "New York"]
+[Site "New York"]
+[Date "1931.??.??"]
+[Round "7"]
+[White "unknown"]
+[Black "Kashdan, Isaac"]
+[Result "0-1"]
+[WhiteElo ""]
+[BlackElo ""]
+[ECO "D61"]
+
+1.d4 Nf6 2.Nf3 e6 3.c4 d5 4.Bg5 Nbd7 5.Nc3 c6 6.Qc2 Be7 7.e3 O-O 8.a3 Re8
+9.Bd3 dxc4 10.Bxc4 Nd5 11.Bxe7 Qxe7 12.Rd1 Nxc3 13.Qxc3 b6 14.O-O Bb7 15.e4 c5
+16.d5 exd5 17.Bxd5 Bxd5 18.exd5 Qd6 19.Nd2 Qxd5 20.Nc4 Qc6 21.Nd6 Red8 22.Rd3 Nf6
+23.Rfd1 Rd7 24.Qc4 Qc7 25.h3 Kf8 26.b4 Rad8 27.Nb5 Qb8 28.Rxd7 Rxd7 29.Rxd7 Nxd7
+30.bxc5 Nxc5 31.Qd5 a6 32.Nd6 Ne6 33.f4 Qc7 34.Qa8+ Ke7 35.Nc8+ Kd7 36.f5 Nd8
+37.Na7 Qb7 38.Nc8 Qxa8 39.Nxb6+ Kc6 40.Nxa8 Kb7 41.Kf2 Kxa8 42.Ke3 Kb7 43.f6 g6
+44.Kf4 Ne6+ 45.Ke5 Kc6  0-1
+
+[Event "New York"]
+[Site "New York"]
+[Date "1931.??.??"]
+[Round "8"]
+[White "unknown"]
+[Black "Dake, Arthur William"]
+[Result "1-0"]
+[WhiteElo ""]
+[BlackElo ""]
+[ECO "D36"]
+
+1.d4 Nf6 2.Nf3 d5 3.c4 c6 4.Nc3 e6 5.Bg5 Nbd7 6.cxd5 exd5 7.e3 Be7 8.Qc2 O-O
+9.Bd3 Re8 10.O-O h6 11.Bf4 Nf8 12.Rae1 Nh5 13.Bg3 Nxg3 14.hxg3 Bf6 15.a3 Ne6
+16.Ne2 Ng5 17.Nd2 Qd6 18.Nf4 g6 19.Bxg6 fxg6 20.Qxg6+ Kf8 21.Qxh6+ Ke7 22.e4 Rh8
+23.Ng6+  1-0
+
+[Event "New York"]
+[Site "New York"]
+[Date "1931.??.??"]
+[Round "9"]
+[White "unknown"]
+[Black "Capablanca, Jose Raul"]
+[Result "0-1"]
+[WhiteElo ""]
+[BlackElo ""]
+[ECO "E16"]
+
+1.Nf3 Nf6 2.d4 e6 3.c4 b6 4.g3 Bb7 5.Bg2 Bb4+ 6.Bd2 Bxd2+ 7.Nbxd2 O-O 8.O-O c5
+9.dxc5 bxc5 10.Rc1 Qc7 11.Nb3 d6 12.Qd2 Nc6 13.Rfd1 Rfd8 14.Nh4 a5 15.a4 Rab8
+16.Rc3 Ba8 17.h3 Nb4 18.Bxa8 Rxa8 19.Qf4 Qc6 20.Rf3 Rd7 21.g4 Qxa4 22.Rxd6 Nbd5
+23.Qe5 Rxd6 24.Qxd6 Ne4 25.Qe5 Qxc4 26.Rd3 a4 27.f3 Nef6 28.Nd2 Qc1+ 29.Kf2 h6
+30.f4 c4 31.Rd4 c3 32.bxc3 a3 33.g5 a2 34.Nb3 Qxc3 35.gxf6 Qxb3 36.Rd1 Qxd1  0-1
+
+[Event "New York"]
+[Site "New York"]
+[Date "1931.??.??"]
+[Round "10"]
+[White "unknown"]
+[Black "Lasker, Edward"]
+[Result "1-0"]
+[WhiteElo ""]
+[BlackElo ""]
+[ECO "C48"]
+
+1.e4 e5 2.Nf3 Nc6 3.Nc3 Nf6 4.Bb5 Nd4 5.Nxd4 exd4 6.e5 dxc3 7.exf6 Qxf6 8.dxc3 Qe5+
+9.Be2 Bc5 10.O-O O-O 11.Bd3 d5 12.Qf3 Bd6 13.g3 c6 14.Be3 Qe6 15.Rfe1 Qg4
+16.Qg2 Qh5 17.f3 f5 18.f4 Qf7 19.Rad1 Re8 20.Qf2 b6 21.b4 Bb7 22.Bd4 Rac8
+23.a3 h5 24.Qf1 c5 25.Be5 Bf8 26.Bb5 Red8 27.Ba6 Bxa6 28.Qxa6 d4 29.Qd3 cxb4
+30.axb4 Rxc3 31.Qxc3 dxc3 32.Rxd8 Kh7 33.Red1 Bxb4 34.R8d7 Bc5+ 35.Kg2 Qc4
+36.Rxg7+ Kh6 37.Rd8 Qe4+ 38.Kh3 Qxe5 39.fxe5 Kxg7 40.Rd7+ Kg6 41.Rxa7 Bd4
+42.e6 Kf6 43.e7 Kf7 44.Kh4 Bg1 45.Rc7 Bxh2 46.Rxc3 Kxe7 47.Kg5 f4 48.gxf4 h4
+49.Rh3  1-0
+
+[Event "New York"]
+[Site "New York"]
+[Date "1931.??.??"]
+[Round "11"]
+[White "Horowitz, Israel Albert"]
+[Black "unknown"]
+[Result "1-0"]
+[WhiteElo ""]
+[BlackElo ""]
+[ECO "A09"]
+
+1.Nf3 d5 2.c4 Nf6 3.cxd5 Nxd5 4.e4 Nf6 5.Nc3 Nc6 6.Bc4 e6 7.d4 Bb4 8.Qd3 Na5
+9.Bb5+ Bd7 10.Bg5 a6 11.Bxd7+ Qxd7 12.O-O O-O-O 13.Rac1 h6 14.Bf4 Nc6 15.a3 Bxc3
+16.Rxc3 g5 17.Rfc1 gxf4 18.Rxc6 Kb8 19.Rxc7 Rc8 20.R7c3 Rxc3 21.Rxc3 Rd8
+22.Qd2 Nxe4 23.Qxf4+ Nd6 24.h3 Ka8 25.Qxh6 Qb5 26.Qg5 Qb6 27.Qc1 Rg8 28.b4 Qb5
+29.Qf4 Nf5 30.Qe4 Qd7 31.Ne5 Qxd4 32.Qxd4 Nxd4 33.Rc4 Nb5 34.Nxf7 Nxa3 35.Re4 Nc2
+36.h4 Rg7 37.Ng5 e5 38.Nf3 Re7 39.h5 b5 40.g4 a5 41.bxa5 b4 42.Rxe5 Rb7 43.Nd2 b3
+44.Nxb3 Rxb3 45.h6 Nd4 46.Re8+ Kb7 47.h7 Nf3+ 48.Kf1 Rb1+ 49.Kg2 Nh4+ 50.Kg3  1-0
+
+
+
+[Event "Prague ol"]
+[Site "Prague"]
+[Date "1931.??.??"]
+[Round "3"]
+[White "unknown"]
+[Black "Steiner, Lajos"]
+[Result "0-1"]
+[WhiteElo ""]
+[BlackElo ""]
+[ECO "D16"]
+
+1.d4 d5 2.Nf3 Nf6 3.c4 c6 4.Nc3 dxc4 5.a4 Bf5 6.e3 Na6 7.Bxc4 Nb4 8.O-O e6
+9.Qe2 Be7 10.Ne5 h6 11.e4 Bh7 12.Rd1 O-O 13.Bf4 Qa5 14.Rac1 Rfd8 15.Qf3 c5
+16.d5 exd5 17.exd5 Bd6 18.Be2 Bxe5 19.Bxe5 Nd7 20.Bf4 Nc2 21.Qg3 Nd4 22.Bc7 Qxc3
+23.Qg4 Qxb2 24.Bc4 Nf6 25.Qg3 Rxd5 26.Re1 Rg5 27.Qe3 Be4 28.Bg3 Nf5 29.Qf4 Nxg3
+30.hxg3 Rf5 31.Qe3 Ng4 32.Qe2 Qxe2 33.Rxe2 Bd5 34.Bd3 Re5 35.Rxc5 Rxe2 36.Bxe2 Nf6
+37.Bd3 Bc6 38.a5 Rd8 39.Bc2 Rd5  0-1
+
+[Event "Prague ol"]
+[Site "Prague"]
+[Date "1931.??.??"]
+[Round "4"]
+[White "Gilg, Karl"]
+[Black "unknown"]
+[Result "0-1"]
+[WhiteElo ""]
+[BlackElo ""]
+[ECO "C42"]
+
+1.e4 e5 2.Nf3 Nf6 3.Nc3 Bb4 4.Bc4 O-O 5.O-O Nc6 6.d3 Bxc3 7.bxc3 d5 8.exd5 Nxd5
+9.Re1 Bg4 10.h3 Nxc3 11.Qd2 Bxf3 12.Qxc3 Bd5 13.Rb1 Bxc4 14.Qxc4 Re8 15.Re4 Qd7
+16.Rxb7 Rab8 17.Rxb8 Rxb8 18.Be3 Rb4 19.Qc5 Rxe4 20.dxe4 h6 21.c4 a6 22.Qd5 Qe8
+23.a3 Kh7 24.f4 exf4 25.Bxf4 Ne7 26.Qb7 c6 27.e5 Ng6 28.Bg3 h5 29.h4 Nxe5
+30.Bxe5 Qxe5 31.Qxf7 Qe1+ 32.Qf1 Qxh4 33.Qd3+ Kh6 34.Qe3+ Qg5 35.Qe6+ Qf6
+36.Qe3+ Kh7 37.Qb6 Qa1+ 38.Kh2 Qe5+ 39.Kg1 Qe1+ 40.Kh2 Qh4+ 41.Kg1 Qxc4 42.Qb1+ Kh6
+43.Qb8 Qe4 44.Qd6+ Kh7 45.Qc5 Qd5 46.Qb6 a5 47.Qb1+ Kh6 48.Qc1+ g5 49.Qc3 h4
+50.Qf6+ Kh5 51.a4 c5 52.Qh8+ Kg4 53.Qc8+ Qf5 54.Qc7 h3 55.Qxa5 Qb1+ 56.Kh2 hxg2
+57.Kxg2 Qc2+ 58.Kf1 Kf3 59.Qa8+ Ke3 60.Qe8+ Qe4 61.Qh5 Qf5+ 62.Kg1 c4 63.Qe8+ Kd2
+64.Qd8+ Qd3 65.Qa5+ c3 66.Qb4 Qg3+  0-1
+
+
+[Event "Prague ol"]
+[Site "Prague"]
+[Date "1931.??.??"]
+[Round "6"]
+[White "Spielmann, Rudolf"]
+[Black "unknown"]
+[Result "1/2-1/2"]
+[WhiteElo ""]
+[BlackElo ""]
+[ECO "D38"]
+
+1.d4 d5 2.c4 e6 3.Nc3 Nf6 4.Bg5 Bb4 5.Nf3 c5 6.cxd5 exd5 7.e3 O-O 8.Bd3 cxd4
+9.Nxd4 Nc6 10.O-O Bxc3 11.bxc3 Ne5 12.Rb1 h6 13.Bh4 a6 14.a4 Re8 15.Be2 g5
+16.Bg3 Ne4 17.Qc2 Qa5 18.Bxe5 Rxe5 19.Bd3 Nc5 20.Nb3 Qc7 21.Nxc5 Qxc5 22.Rb4 Re7
+23.Rd4 Bd7 24.c4 dxc4 25.Rxc4 Qa5 26.Be4 Rb8 27.Qd1 Bf5 28.Qd6 Qe5 29.Qxe5 Rxe5
+30.Bxf5 Rxf5 31.g4  1/2-1/2
+
+
+[Event "Warsaw ol"]
+[Site "Warsaw"]
+[Date "1935.??.??"]
+[Round "?"]
+[White "unknown"]
+[Black "Apsenieks, Fricis"]
+[Result "0-1"]
+[WhiteElo ""]
+[BlackElo ""]
+[ECO "D20"]
+
+1.d4 d5 2.c4 dxc4 3.e3 e5 4.Bxc4 exd4 5.exd4 Nc6 6.Nf3 Bb4+ 7.Nc3 Qe7+ 8.Be3 Nf6
+9.O-O Be6 10.Bxe6 fxe6 11.Qb3 O-O-O 12.Rac1 Ba5 13.Rfe1 Rhe8 14.Ne5 Nxe5
+15.dxe5 Nd5 16.Nb5 Qb4 17.Re2 Qxb3 18.axb3 Nxe3 19.fxe3 Kb8 20.b4 Bb6 21.Na3 a6
+22.Nc4 Ba7 23.Rf1 Rd7 24.Rf3 b5 25.Na5 Bb6 26.Rc2 Bxa5 27.bxa5 Kb7 28.Rh3 Rf8
+29.Rf3 Rfd8 30.b4 Re7 31.Rcf2 Rd1+ 32.Rf1 Rxf1+ 33.Kxf1 c5 34.bxc5 Kc6 35.Rf8 Kxc5
+36.Ra8 Rb7 37.Rxa6 b4 38.Rd6 b3 39.Rd1 Kb4 40.a6 Ra7 41.Ke2 b2 42.Rd6 Ka3
+43.Rb6 Ka2  0-1
+
+[Event "Warsaw ol"]
+[Site "Warsaw"]
+[Date "1935.??.??"]
+[Round "4"]
+[White "unknown"]
+[Black "Stoltz, Goesta"]
+[Result "0-1"]
+[WhiteElo ""]
+[BlackElo ""]
+[ECO "D13"]
+
+1.d4 d5 2.c4 c6 3.Nc3 Nf6 4.cxd5 cxd5 5.Nf3 Nc6 6.Bf4 Qb6 7.Na4 Qa5+ 8.Bd2 Qd8
+9.e3 e6 10.Rc1 Bd6 11.Bb5 Bd7 12.O-O Ne4 13.Nc5 Bxc5 14.dxc5 O-O 15.Bc3 Rc8
+16.a3 Ne5 17.Bxd7 Nxf3+ 18.gxf3 Nxc3 19.Rxc3 Qxd7 20.Qd4 e5 21.Qxe5 Rc6 22.e4 d4
+23.Rd3 Qh3 24.Rxd4 Rg6+ 25.Qg3 f5 26.Rfd1 f4 27.Qxg6 hxg6 28.R1d3 g5 29.e5 g4
+30.fxg4 f3  0-1
